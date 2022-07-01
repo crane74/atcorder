461481279e69f4b4af0754eb42eba71dbed21da8
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-07-01 22:53:08 +0900

    0701__2題

---
+++ c++/abc/144/A[9x9].cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  if (A > 9 || B > 9) {
+    cout << -1 << endl;
+  } else {
+    cout << A * B << endl;
+  }
+}
