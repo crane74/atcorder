b76297a6f5cc95098d95124701375157fdae6df9
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-07-05 22:14:18 +0900

    0705__1題

---
+++ c++/abc/105/A[Atcoder_Crackers].cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+
+  if (N % K == 0) {
+    cout << 0 << endl;
+  }
+
+  else {
+    cout << 1 << endl;
+  }
+}
