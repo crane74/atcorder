03312c93e41348adaf4bc36b134773b52c0e5d5f
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-07-10 22:59:41 +0900

    0710__3題

---
+++ c++/abc/112/B[Time_Limit_Exceeded].cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  int N, T;
+  cin >> N >> T;
+
+  int min = 1001;
+
+  vector<pair<int, int>> cost_time(N);
+  rep(i, N) {
+    cin >> cost_time[i].first >> cost_time[i].second;
+    if (cost_time[i].second <= T && cost_time[i].first < min) {
+      min = cost_time[i].first;
+    }
+  }
+  if (min == 1001) {
+    cout << "TLE" << endl;
+  } else {
+    cout << min << endl;
+  }
+}
