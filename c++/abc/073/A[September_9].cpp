248f86151887bb27a07d50608df8cd90d77087ff
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-07-11 23:27:55 +0900

    0711__5題

---
+++ c++/abc/073/A[September_9].cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+void test(int num, bool &ans) {
+  while (num > 0) {
+    if (num % 10 == 9) {
+      ans = true;
+    }
+    num /= 10;
+  }
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  bool ans = false;
+
+  test(N, ans);
+  YesNo(ans);
+}
---
+++ c++/abc/073/B[Theater].cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int ans = 0;
+
+  vector<pair<int, int>> D(N);
+  for (int i = 0; i < N; i++) {
+    cin >> D[i].first >> D[i].second;
+    ans += (D[i].second - D[i].first + 1);
+  }
+
+  cout << ans << endl;
+}
