cf851604c921ae372634c37171e5cf0eaad98b77
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-08-01 21:32:54 +0900

    0801__2題

---
+++ c++/abc/147/A[Blackjack].cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define fore(i, a) for (auto &i : a)
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+
+  rep(i, 3) {
+    int a;
+    cin >> a;
+    sum += a;
+  }
+
+  if (sum >= 22)
+    cout << "bust" << endl;
+  else
+    cout << "win" << endl;
+}
---
+++ c++/abc/147/B[Palindrome-philia].cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define fore(i, a) for (auto &i : a)
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  string S_reversed;
+  S_reversed = S;
+  reverse(S_reversed.begin(), S_reversed.end());
+
+  int ans = 0;
+  rep(i, S.size()) {
+    if (S[i] != S_reversed[i]) ans++;
+  }
+
+  cout << ans / 2 << endl;
+}
