e752d5279bc55b5d4c8ef76d5a3aa07362ecadd9
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-08-03 23:07:13 +0900

    0803__1題

---
+++ c++/abc/133/C[Remainder_Minimization_2019].cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define fore(i, a) for (auto &i : a)
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  ll L, R;
+  cin >> L >> R;
+
+  L = L % 2019;
+  R = R % 2019;
+
+  int ans = 2019;
+  for (int i = L; i < R; i++) {
+    for (int j = i + 1; j <= R; j++) {
+      int mn = (i * j) % 2019;
+
+      if (mn < ans) ans = mn;
+    }
+  }
+
+  cout << ans << endl;
+}
