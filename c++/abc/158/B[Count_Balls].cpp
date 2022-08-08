3d3e4c2cb25a9d97f1c315373342d1eb8c40436b
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-08-08 22:31:51 +0900

    0808__2題

---
+++ c++/abc/158/B[Count_Balls].cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define fore(i, a) for (auto &i : a)
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  ll N, A, B;
+  cin >> N >> A >> B;
+
+  ll len = A + B;
+  ll ans = (N / len) * A + min(A, N % len);
+
+  cout << ans << endl;
+}
