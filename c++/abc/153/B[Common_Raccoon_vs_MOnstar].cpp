54cc3f3f9d0c54ce9b9d1611fd79ab0f9a19aba1
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-08-18 23:33:35 +0900

    0818__3題

---
+++ c++/abc/153/B[Common_Raccoon_vs_MOnstar].cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+// REP macro
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i, n) for (ll i = n - 1; i >= 0; i--)
+#define rrepd(i, n) for (ll i = n; i >= 1; i--)
+
+// define short
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+#define fore(i, a) for (auto &i : a)
+#define YesNo(bool)        \
+  if (bool) {              \
+    cout << "Yes" << endl; \
+  } else {                 \
+    cout << "No" << endl;  \
+  }
+
+// alias
+using ull = unsigned long long;
+using ll = long long;
+
+using namespace std;
+
+int main() {
+  ll H, N;
+  cin >> H >> N;
+
+  vector<ll> A(N);
+  rep(i, N) cin >> A[i];
+
+  bool ans = false;
+
+  rep(i, N) {
+    H -= A[i];
+    if (H <= 0) {
+      ans = true;
+      break;
+    }
+  }
+
+  YesNo(ans);
+}
